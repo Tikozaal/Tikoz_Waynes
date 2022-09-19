33d02ccf3f8117a6e3965d9bdb956d3ec3b7fa8d
Tikoz <106456046+Tikozaal@users.noreply.github.com>
2022-09-19 17:17:24 +0200

    Update server.lua

--- Tikoz_Waynes/server.lua
+++ Tikoz_Waynes/server.lua
@@ -248,7 +248,7 @@ AddEventHandler("Tikoz:waynesdepotentreprise", function(money)
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
     local total = money
-    local xMoney = xPlayer.getMoney()
+    local xMoney = xPlayer.getAccount("bank").money
     
     TriggerEvent('esx_addonaccount:getSharedAccount', "society_waynes", function (account)
         if xMoney >= total then
